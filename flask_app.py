7066d2a67f1d1d1fac9991e2d0df225667b67847
Flumby124 <yannick.studer@gmx.net>
2026-01-01 20:52:29 +0000

    test

--- flask_app.py
+++ flask_app.py
@@ -46,6 +46,7 @@ def is_valid_signature(x_hub_signature, data, private_key):
     mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
     return hmac.compare_digest(mac.hexdigest(), github_signature)
 
+print(">>> LOADING ROUTE: /update_server")
 @app.post('/update_server')
 def webhook():
     x_hub_signature = request.headers.get('X-Hub-Signature')
@@ -58,7 +59,7 @@ def webhook():
 
 
 
-
+print(">>> LOADING ROUTE: /login")
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
@@ -86,7 +87,7 @@ def login():
         footer_link_label="Registrieren"
     )
 
-
+print(">>> LOADING ROUTE: /register")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     error = None
@@ -112,6 +113,7 @@ def register():
         footer_link_label="Einloggen"
     )
 
+print(">>> LOADING ROUTE: /logout")
 
 @app.route("/logout")
 @login_required
@@ -121,6 +123,7 @@ def logout():
 
 
 
+print(">>> LOADING ROUTE: /")
 
 @app.route("/")
 @login_required
@@ -136,6 +139,7 @@ def index():
 
     return render_template("dashboard.html", pcs=pcs, sales=sales)
 
+print(">>> LOADING ROUTE: /pcs")
 
 @app.route("/pcs")
 @login_required
@@ -143,6 +147,7 @@ def pc_list():
     pcs = db_read("SELECT id, name, status, gesamtpreis FROM pc ORDER BY id DESC")
     return render_template("pc_list.html", pcs=pcs)
 
+print(">>> LOADING ROUTE: /pc/new")
 
 @app.route("/pc/new", methods=["GET", "POST"])
 @login_required
@@ -156,6 +161,7 @@ def pc_new():
 
     return render_template("pc_new.html")
 
+print(">>> LOADING ROUTE: /pc/<int:pc_id")
 
 @app.route("/pc/<int:pc_id>")
 @login_required
@@ -165,7 +171,7 @@ def pc_detail(pc_id):
     return render_template("pc_detail.html", pc=pc, komponenten=komponenten)
 
 
-
+print(">>> LOADING ROUTE: /components/new/<typ>")
 
 @app.route("/components/new/<typ>", methods=["GET", "POST"], endpoint="component_new_page")
 @login_required
